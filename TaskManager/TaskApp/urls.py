27a2a635175ad7597f540fc20d5839c8d861c606
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-15 15:09:40 +0330

    all paths in urlpatterns added

--- TaskManager/TaskApp/urls.py
+++ TaskManager/TaskApp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('search/',),
-    path('tasks/',),
-    path('tasks/<int:post_id>',)
+    path('search/', views.search, name='search'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('tasks/<int:post_id>', views.task_detail, name='task_detail')
     
 ]
