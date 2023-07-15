31946f0a011bc540eed3d77a482a54ba157f8e25
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-15 14:43:23 +0330

    chore: path for home completed

--- TaskManager/TaskApp/urls.py
+++ TaskManager/TaskApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',),
+    path('', views.home, name='home'),
     path('search/',),
     path('tasks/',),
     path('tasks/<int:post_id>',)
