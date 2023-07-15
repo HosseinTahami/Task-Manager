7ecd25a9524c8c73b7809c5eefb3d3217faad2bc
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-15 19:32:45 +0330

    task_detail Completed

--- TaskManager/TaskApp/urls.py
+++ TaskManager/TaskApp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
     path('tasks/', views.tasks, name='tasks'),
-    path('tasks/<int:post_id>', views.task_detail, name='task_detail')
+    path('tasks/<int:task_id>', views.task_detail, name='task_detail')
     
 ]
--- TaskManager/TaskApp/views.py
+++ TaskManager/TaskApp/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from .models import Category, Tag, Task
+from  datetime import datetime
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def search(request):
+    pass
     return render(request, 'search.html')
 
-def task_detail(request):
-    return render(request, 'task_detail.html')
+def task_detail(request, task_id):
+    detail = Task.objects.get(id = task_id)
+    now = timezone.now()
+    if detail.due_date > now :
+        remain = 'Ongoing'
+    else:
+        remain = 'Finished'
+    return render(request, 'task_detail.html', {'detail' : detail , 'remain' : remain})
 
 def tasks(request):
     all_tasks = Task.objects.all()
