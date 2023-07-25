6533a33b01fc5db9929ad204a71934abaee81ed7
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-25 15:34:26 +0330

    models.py of authuser completed but ...

--- TaskManager/authuser/models.py
+++ TaskManager/authuser/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
-
+from django.utils import timezone
 class CustomUserManager(UserManager):
     def _create_user(self, email, password, **extra_fields):
         if not email:
@@ -28,4 +28,28 @@ class CustomUserManager(UserManager):
         return self._create_user(email, password, **extra_fields)
     
 class User(AbstractBaseUser, PermissionsMixin):
-    pass
+    email = models.EmailField(blank=True, default='', unique=True)
+    name = models.CharField(max_length=225, blank=True, default='')
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    
+    objects = CustomUserManager()
+    USERNAME_FIELDS = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
+    def get_full_name(self):
+        return self.name
+    
+    def get_short_name(self):
+        return self.name or self.email.split('@')[0]
+    
+        
