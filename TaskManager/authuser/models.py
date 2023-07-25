6b7f038a0f4c1fe0bd2a2d8ca2d997dbd8766eb2
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-25 15:04:20 +0330

    a class added for creating user

--- TaskManager/authuser/models.py
+++ TaskManager/authuser/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import UserManager
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("No Email provided")
+        
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
