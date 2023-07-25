a11e3d6c7706dc7c365802bf1bdfafd6ecf01d2b
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-25 15:15:19 +0330

    I have no Idea What is happening

--- TaskManager/authuser/models.py
+++ TaskManager/authuser/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
 
 class CustomUserManager(UserManager):
-    def create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("No Email provided")
         
@@ -14,3 +14,18 @@ class CustomUserManager(UserManager):
         user.save(using=self._db)
         
         return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        return self._create_user(email, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    pass
