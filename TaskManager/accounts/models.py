5046d240daf24140d610566e32c1fae7972dee59
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-03 16:07:44 +0330

    fix: User --> Custom

--- TaskManager/accounts/models.py
+++ TaskManager/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     photo = models.ImageField(upload_to='images')
     
     
