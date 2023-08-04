2effc84fea10658b3ec3668207c0da291327b46a
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-04 06:23:14 +0330

    feat: CustomUserCreationForm Created

--- TaskManager/accounts/forms.py
+++ TaskManager/accounts/forms.py
@@ -5,8 +5,18 @@ from .models import CustomUser
 #from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
-
-
+class CustomUserCreationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+        )
+    confirm_password = forms.CharField(
+        label='Confirm Password',
+        widget= forms.PasswordInput
+    )
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
 
 
 
