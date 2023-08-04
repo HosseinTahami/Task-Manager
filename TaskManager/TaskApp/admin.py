c6f9c306f468a9b5ef33fb32bb922ff33c053e7e
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-04 07:19:39 +0330

    fix: different type of imports

--- TaskManager/TaskApp/admin.py
+++ TaskManager/TaskApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import *
+from . models import Task, Category, Tag
 # Register your models here.
 admin.site.register(Task)
 admin.site.register(Category)
