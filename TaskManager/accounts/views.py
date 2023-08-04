72bd33f4a9d849a9ce34cc32b64306fbf7c52058
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-04 14:20:11 +0330

    feat: Register/Login/ ... view

--- TaskManager/accounts/views.py
+++ TaskManager/accounts/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import CustomUserCreationForm
 # Create your views here.
 
 class LoginView(View):
-    def get(request):
+    def get(self, request):
         pass
-    def post(request):
+    def post(self, request):
         pass
-    
+
+class RegisterView(View):
+    class_form = CustomUserCreationForm
+    template_name = 'register.html'
+    def get(self, request):
+        form = self.class_form
+        return render(request, self.template_name, {'form':form})
+    def post(self, request):
+        form = self.class_form(request.POST)
+        if form.is_valid:
+            form.save()
+        return redirect('home')
