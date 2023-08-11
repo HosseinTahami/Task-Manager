2e24af01d21fc4d5491be8c29da5a5a57b39c6fc
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-11 10:20:39 +0330

    add url

--- TaskManager/TaskApp/urls.py
+++ TaskManager/TaskApp/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from . import views
 
-app_name='TaskApp'
+app_name = "TaskApp"
 
 urlpatterns = [
-    path('', views.main_page, name='main_page'),
-    path('home/', views.home, name='home'),
-    path('search/', views.search, name='search'),
-    path('tasks/', views.tasks, name='tasks'),
-    path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
-    path('search_results/', views.search_results, name='search_results'),
-    path('category/', views.category, name='category'),
-    path('category/<int:category_id>', views.category_detail, name='category_detail'),
-    path('emergency/', views.emergency_tasks, name='em')
+    path("", views.main_page, name="main_page"),
+    path("home/", views.home, name="home"),
+    path("search/", views.search, name="search"),
+    path("tasks/", views.tasks, name="tasks"),
+    path("tasks/<int:task_id>", views.TaskDetail.as_view(), name="task_detail"),
+    path("search_results/", views.search_results, name="search_results"),
+    path("category/", views.category, name="category"),
+    path("category/<int:category_id>", views.category_detail, name="category_detail"),
+    path("emergency/", views.emergency_tasks, name="em"),
 ]
