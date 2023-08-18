64f733066d2b691bf98ea25c18daa1bb77b87696
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-18 08:51:06 +0330

    CreateTask created

--- TaskManager/TaskApp/views.py
+++ TaskManager/TaskApp/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.db import models
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from .models import Task, Category, Tag
 from accounts.models import CustomUser
 from django.utils import timezone
@@ -8,10 +9,7 @@ from django.db.models import Q
 from datetime import datetime, time
 from django.views import View
 from .mixins import TodoOwnerRequiredMixin
-from django.views.generic import (
-    ListView,
-    DetailView,
-)
+from django.views.generic import ListView, DetailView, CreateView
 
 
 def main_page(request):
@@ -34,8 +32,17 @@ def search(request):
 class TaskDetail(DetailView):
     template_name = "task_detail.html"
     model = Task
-    context_object_name = "tasks"
+    context_object_name = "task"
     pk_url_kwarg = "task_id"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        task = Task.objects.filter(id=self.kwargs["task_id"])
+        print(task[0].tags)
+        # context["tags"] = task[0].tags
+        # context["tags"] = task[0].tags
+        # print(context["tags"])
+        return context
 
 
 # class TaskDetail(TodoOwnerRequiredMixin, View):
@@ -71,49 +78,49 @@ class TaskDetail(DetailView):
 #         )
 
 
-class Tasks(TodoOwnerRequiredMixin, View):
-    def post(self, request):
-        title = request.POST["title"]
-        description = request.POST["description"]
-        due_date = request.POST["due_date"]
-        status = request.POST["status"]
-        category = request.POST["category"]
-        category = Category.objects.get(name=category)
-        tags_list = request.POST.getlist("tags")
-        task_file = request.FILES.get("task_file")
-        # if not task_file:
-        #     default_image_path = os.path.join(settings.MEDIA_ROOT, 'task_default.png')
-        #     with open(default_image_path, 'rb') as f:
-        #         task_file = File(f, name='task_default.png')
-        new_task = Task.objects.create(
-            title=title,
-            description=description,
-            due_date=due_date,
-            status=status,
-            category=category,
-            file=task_file,
-        )
-        for t in tags_list:
-            tag_obj = Tag.objects.get(name=t)
-            new_task.tags.add(tag_obj)
-            new_task.save()
+# class Tasks(TodoOwnerRequiredMixin, View):
+#     def post(self, request):
+#         title = request.POST["title"]
+#         description = request.POST["description"]
+#         due_date = request.POST["due_date"]
+#         status = request.POST["status"]
+#         category = request.POST["category"]
+#         category = Category.objects.get(name=category)
+#         tags_list = request.POST.getlist("tags")
+#         task_file = request.FILES.get("task_file")
+#         # if not task_file:
+#         #     default_image_path = os.path.join(settings.MEDIA_ROOT, 'task_default.png')
+#         #     with open(default_image_path, 'rb') as f:
+#         #         task_file = File(f, name='task_default.png')
+#         new_task = Task.objects.create(
+#             title=title,
+#             description=description,
+#             due_date=due_date,
+#             status=status,
+#             category=category,
+#             file=task_file,
+#         )
+#         for t in tags_list:
+#             tag_obj = Tag.objects.get(name=t)
+#             new_task.tags.add(tag_obj)
+#             new_task.save()
 
-        return redirect("home")
+#         return redirect("home")
 
-    def get(self, request):
-        all_tags = Tag.objects.all()
-        all_tasks = Task.objects.all()
-        all_category = Category.objects.all()
-        return render(
-            request,
-            "tasks.html",
-            {
-                "tasks": list(all_tasks),
-                "category": all_category,
-                "tags": all_tags,
-                "status": Task.objects.all(),
-            },
-        )
+#     def get(self, request):
+#         all_tags = Tag.objects.all()
+#         all_tasks = Task.objects.all()
+#         all_category = Category.objects.all()
+#         return render(
+#             request,
+#             "tasks.html",
+#             {
+#                 "tasks": list(all_tasks),
+#                 "category": all_category,
+#                 "tags": all_tags,
+#                 "status": Task.objects.all(),
+#             },
+#         )
 
 
 def search_results(request):
@@ -222,3 +229,17 @@ def emergency_tasks(request):
     tmp = datetime.combine(datetime.now(), time.max)
     tasks = Task.objects.filter(Q(due_date__lt=tmp) & Q(status="O"))
     return render(request, "em.html", {"tasks": tasks})
+
+
+class CreateTaskView(CreateView):
+    model = Task
+    fields = "__all__"
+    template_name = "create_task.html"
+    success_url = reverse_lazy("home")
+
+
+class Tasks(ListView):
+    model = Task
+    context_object_name = "tasks"
+    template_name = "tasks.html"
+    pk_url_kwarg = "task_id"
